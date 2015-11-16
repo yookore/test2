be9a7d57e66be04cee2ef38872fa0665a6135ef4
Jome Akpoduado <jomea@yookos.com>
2015-11-16 13:53:16 +0200

    rtest

--- su_app/views.py
+++ su_app/views.py
@@ -1,40 +1,37 @@
-from django.shortcuts import render
-
 # Create your views here.
 
 from rest_framework import (
-	authentication,
-	permissions,
-	viewsets,
-	status
+    viewsets
 )
-from rest_framework.response import Response
 
 from models import StatusUpdate
 from serializers import StatusUpdateSerializer
 from pagination import CustomPagination
 
+
 class DefaultsMixin(object):
-    paginate_by         = 25
-    paginate_by_param   = 'page_size'
-    max_paginate_by     = 100
-    pagination_class	= CustomPagination
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+    pagination_class = CustomPagination
 
 
 class StatusUpdateViewSet(DefaultsMixin, viewsets.ModelViewSet):
-	""" API endpoint for listing and creating status updates """
+    """ API endpoint for listing and creating status updates """
 
-	queryset 			= StatusUpdate.objects.all()
-	serializer_class 	= StatusUpdateSerializer
+    queryset = StatusUpdate.objects.all()[:1]
+    serializer_class = StatusUpdateSerializer
 
-from django.http import HttpResponse, HttpResponseRedirect
+
+from django.http import HttpResponse
+
 
 def home(request):
     from yookosusers import YookosUser
     user = YookosUser()
     res = user.get_upm('jomski2009')
     if res:
-        response = res['username'] +'</br>' +res['firstname'] +'</br>' + res['lastname']
+        response = res['username'] + '</br>' + res['firstname'] + '</br>' + res['lastname']
     else:
         response = 'Could not get the profile'
     return HttpResponse(response)
